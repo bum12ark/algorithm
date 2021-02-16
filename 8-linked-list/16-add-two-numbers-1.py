77ccf90168fcda64ea1faf94a851efb95daa5330
SangBum <bum12ark@gmail.com>
2021-02-17 01:53:30 +0900

    2021-02-16 : home study

--- 8-linked-list/16-add-two-numbers-1.py
+++ 8-linked-list/16-add-two-numbers-1.py
@@ -9,7 +9,6 @@
 342 + 465 = 807
 """
 # Definition for singly-linked list.
-import collections
 from typing import List
 
 
@@ -27,8 +26,19 @@ class ListNode:
 
 class Solution:
     # 연결리스트 뒤집기 -> 재귀 사용
-    def reverseList(self, head: ListNode) -> ListNode:
-        pass
+    def reverseListRecursive(self, current: ListNode, prev: ListNode = None) -> ListNode:
+        if not current:
+            return prev
+        next, current.next = current.next, prev
+        return self.reverseListRecursive(next, current)
+
+    # 연결리스트 뒤집기 -> 반복문 사용
+    def reverseListLoop(self, current: ListNode) -> ListNode:
+        prev = None
+        while current:
+            next, current.next = current.next, prev
+            prev, current = current, next
+        return prev
 
     # 연결 리스트를 파이썬 리스트로 변환
     def toList(self, node: ListNode) -> List:
@@ -38,19 +48,28 @@ class Solution:
             node = node.next
         return li
 
-    # 파이썬 리스트를 연결 리스트로 변환
+    # 파이썬 리스트를 연결 리스트로 변환 (역순)
     def toReversedLinkedList(self, result: str) -> ListNode:
         prev = None
         for s in result:
-            pass
+            node = ListNode(s)
+            node.next = prev
+            prev = node
+        return node
 
     # 두 연결 리스트의 덧셈
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        pass
+        # 연결리스트 뒤집기
+        list1: List = self.toList(self.reverseListRecursive(l1))
+        list2: List = self.toList(self.reverseListLoop(l2))
+        sum_num: int = int(''.join(str(n) for n in list1)) + int(''.join(str(n) for n in list2))
+
+        # 최종 계산 결과 연결 리스트 변환
+        return self.toReversedLinkedList(str(sum_num))
 
 
 if __name__ == '__main__':
     solution = Solution()
     param1 = ListNode(2, ListNode(4, ListNode(3)))
     param2 = ListNode(5, ListNode(6, ListNode(4)))
-    solution.addTwoNumbers(param1, param2)
+    print(solution.addTwoNumbers(param1, param2).print_list())
---
+++ 8-linked-list/17-swap-nodes-in-pairs-3.py
@@ -0,0 +1,23 @@
+"""
+* 페어의 노드스왑
+연결 리스트를 입력받아 페어 단위로 스왑하라.
+- 입력
+1->2->3->4
+- 출력
+2->1->4->3
+"""
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def print_list(self):
+        cur = self
+        while cur:
+            print(cur.val, end='->')
+            cur = cur.next
+
+class Solution:
+    # 재귀 구조로 스왑
+    def swapPairs(self, head: ListNode) -> ListNode:
+        pass
