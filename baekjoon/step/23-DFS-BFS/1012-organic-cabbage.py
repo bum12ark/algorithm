79c231969d1bcfc1fb192536aca62fa137196019
bum12ark <bum12ark@gmail.com>
2021-05-04 17:58:36 +0900

    Author: SangBum Park <bum12ark@gmail.com>
    - DFS/BFS 풀이 중

--- baekjoon/step/23-DFS-BFS/1012-organic-cabbage.py
+++ baekjoon/step/23-DFS-BFS/1012-organic-cabbage.py
@@ -1,6 +1,7 @@
 """
 출처: https://www.acmicpc.net/problem/1012
 """
+import collections
 import sys
 from typing import List
 
@@ -21,6 +22,25 @@ def dfs(x: int, y: int, grid: List[int]) -> None:
     dfs(x, y - 1, grid)
 
 
+def bfs(x: int, y: int, grid: List[int]) -> None:
+    ewsn_x = [1, -1, 0, 0]  # 동서남북 x
+    ewns_y = [0, 0, 1, -1]  # 동서남북 y
+
+    deq = collections.deque([(x, y)])
+    while deq:
+        pop_x, pop_y = deq.popleft()
+        for idx in range(4):
+            row = pop_x + ewsn_x[idx]
+            col = pop_y + ewns_y[idx]
+
+            # 진행 조건
+            if 0 <= row < len(grid) and \
+                    0 <= col < len(grid[row]) and \
+                    grid[row][col] == 1:
+                grid[row][col] = 0  # 방문 표시
+                deq.append((row, col))
+
+
 def nums_cabbage(grid: List[int]) -> int:
     # 예외 처리
     if not grid:
@@ -30,7 +50,8 @@ def nums_cabbage(grid: List[int]) -> int:
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                dfs(i, j, grid)
+                # dfs(i, j, grid)
+                bfs(i, j, grid)
                 # 모든 배추밭 탐색 후 카운트 증가
                 count += 1
     return count
@@ -52,3 +73,14 @@ if __name__ == '__main__':
             field[b][a] = 1
 
         print(nums_cabbage(field))
+
+"""
+1
+5 3 6
+0 2
+1 2
+2 2
+3 2
+4 2
+4 0
+"""
